f00e4bd6c563575c8cd9ebc400c0fdb8eab5f3bc
mdshaharulsiyam <shaharulsiyam0273@gmail.com>
2024-11-16 16:58:48 +0600

    s

--- src/app/(withCommonLayout)/all-cars/page.tsx
+++ src/app/(withCommonLayout)/all-cars/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-import Map from "@/components/Map/Map";
+import MapParent from "@/components/Map/MapParent";
 import React from "react";
-
-
 const AllCarsPage = () => {
   return (
     <div className="container mx-auto">
       <div className="p-4 bg-white shadow-sm flex flex-wrap items-center space-x-4 rounded-lg">
-     
-      <Map center={[51.505, -0.09]} zoom={2} />
       </div>
+        <MapParent />
     </div>
   )
 }
