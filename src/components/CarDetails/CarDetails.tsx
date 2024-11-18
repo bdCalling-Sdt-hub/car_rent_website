3b06f96056ee6c9b9309b78a2db3ec4a48eb57b9
shukumar-542 <shukumar542@gmail.com>
2024-11-18 11:24:51 +0600

    Implement car details page

--- src/components/CarDetails/CarDetails.tsx
+++ src/components/CarDetails/CarDetails.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import HeadingTitle from '../shared/HeadingTitle'
 import { Checkbox } from '../ui/checkbox';
+import { Textarea } from '../ui/textarea';
 const features = [
     "All-wheel drive", "AUX input", "Blind spot warning", "Convertible", "Keyless entry", "Snow tires", "USB Charger",
     "Android Auto", "Backup camera", "Bluetooth", "GPS", "Pet friendly", "Sunroof", "USB input",
@@ -27,12 +28,25 @@ const CarDetails = () => {
                             <Checkbox
                                 value={feature}
                                 checked={selectedFeatures.includes(feature)}
-                                onChange={() => handleCheckboxChange(feature)}
+                                onCheckedChange={() => handleCheckboxChange(feature)}
                                 className="h-4 w-4 "
                             />
                             <span className="text-gray-700">{feature}</span>
                         </label>
                     ))}
+                </div>
+                    <p className='text-sm text-gray-600 mt-5'>Apple CarPlay is a registrar trademark of apple inc. Android is a trademark of google LLC.  </p>
+                {/* Description Section */}
+                <div className='mt-10'>
+                    <h3 className="text-lg font-semibold">Description</h3>
+                    <p className="text-sm text-gray-600">
+                        Tell guests what makes your car unique and why they'll love driving it.
+                    </p>
+                    <Textarea
+                        placeholder="No need to include your contact info or pickup instructions. Guests will receive those once they book your car."
+                        className="mt-2 max-w-6xl"
+                        rows={5}
+                    />
                 </div>
             </div>
         </div>
