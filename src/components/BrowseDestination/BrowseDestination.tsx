8c2bd74f92306890e93050192b6db8ef7d111557
shukumar-542 <shukumar542@gmail.com>
2025-01-05 12:22:58 +0600

    Enhancement overview section

--- src/components/BrowseDestination/BrowseDestination.tsx
+++ src/components/BrowseDestination/BrowseDestination.tsx
@@ -5,7 +5,7 @@ import DestinationCaurosel from '../DestinationCaurosel'
 const BrowseDestination = () => {
   return (
     <div className='container mx-auto md:py-20 py-10 px-2 md:px-0'>
-            <HeadingTitle title={'Browse by Destination'} />
+            <HeadingTitle title={'Available all over the uk'} />
 
             <div className='md:mt-5 mt-2 '>
                 <p className='bg-[#BCBABA26] inline-block rounded-full md:px-4 text-[10px] md:text-[18px] px-2 md:py-2 py-1'>Select your destination </p>
