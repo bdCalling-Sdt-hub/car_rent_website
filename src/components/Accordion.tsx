5af542160e7161a8fed0af38eac06f651d99acc1
shukumar-542 <shukumar542@gmail.com>
2025-01-07 17:27:05 +0600

    Implement FAQ api integration

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,35 +1,43 @@
 import {
-    Accordion as AccordionRoot,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+  Accordion as AccordionRoot,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-const Accordion = () => {
-  return (
-    <AccordionRoot type="single" collapsible className="w-full">
-      <AccordionItem value="item-1" className="border  px-2 mt-5 ">
-        <AccordionTrigger className="text-sm md:text-[20px]">How do i book car?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2" className="border  px-2 mt-5">
-        <AccordionTrigger className="text-sm md:text-[20px]">What documents do i need to rent a car?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3" className="border  px-2 mt-5" >
-        <AccordionTrigger className="text-sm md:text-[20px]">Can i modify cancel a booking car?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It&apos;s animated by default, but you can disable it if you
-          prefer.
-        </AccordionContent>
-      </AccordionItem>
-    </AccordionRoot>
-  )
+interface TFaq {
+  _id: string;
+  question: string;
+  answer: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export default Accordion
+const Accordion = async () => {
+  const response = await fetch("http://192.168.12.75:8056/manage/get-faq", {
+    next: {
+      revalidate: 30,
+    },
+  });
+  const faqs = await response.json();
+  return (
+    <AccordionRoot type="single" collapsible className="w-full">
+      {faqs?.data?.map((faq: TFaq , i : string) => {
+        return (
+          <AccordionItem
+            key={faq?._id}
+            value={`item-${i+1}`}
+            className="border  px-2 mt-5 "
+          >
+            <AccordionTrigger className="text-sm md:text-[20px]">
+              {faq?.question}
+            </AccordionTrigger>
+            <AccordionContent>{faq?.answer}</AccordionContent>
+          </AccordionItem>
+        );
+      })}
+    </AccordionRoot>
+  );
+};
+
+export default Accordion;
