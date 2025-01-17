cc21fcc0ad105102fdb1daf23990dea39ee6aa7f
shukumar-542 <shukumar542@gmail.com>
2025-01-17 09:57:45 +0600

    Implement ongoing car api integrate

--- src/app/(withCommonLayout)/host-history/page.tsx
+++ src/app/(withCommonLayout)/host-history/page.tsx
@@ -1,69 +1,112 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import { CiStar } from 'react-icons/ci'
-import Link from 'next/link'
-import { IoAdd } from 'react-icons/io5'
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-} from "@/components/ui/tabs"
-import HostCars from '@/components/HostCars/HostCars'
-import HostOrder from '@/components/HostOrder/HostOrder'
-import HostHistoryInfo from '@/components/HostHistoryInfo/HostHistoryInfo'
-import HostEarning from '@/components/HostEarning/HostEarning'
-import { useGetProfileQuery } from '@/redux/Api/authApi'
-import { imageUrl } from '@/redux/baseApi'
+"use client";
+import React from "react";
+import Image from "next/image";
+import { CiStar } from "react-icons/ci";
+import Link from "next/link";
+import { IoAdd } from "react-icons/io5";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import HostCars from "@/components/HostCars/HostCars";
+import HostOrder from "@/components/HostOrder/HostOrder";
+import HostHistoryInfo from "@/components/HostHistoryInfo/HostHistoryInfo";
+import HostEarning from "@/components/HostEarning/HostEarning";
+import { useGetProfileQuery } from "@/redux/Api/authApi";
+import { imageUrl } from "@/redux/baseApi";
+import OngoingOrder from "@/components/OngoingOrder/OngoingOrder";
 
 const HostHistoryPage = () => {
-    const {data: getProfile} = useGetProfileQuery(undefined);
-    // console.log(getProfile?.data);
-   
-    return (
-        <div className='my-10 container mx-auto px-2 md:px-0'>
-            <div className='md:flex items-center  justify-between '>
-                <div className='flex items-center gap-5'>
-                    <Image className='rounded-full h-20 w-20 object-cover' src={`${imageUrl}/${getProfile?.data?.profile_image}`} height={200} width={200} alt='img' />
-                    <div className='font-lora space-y-1'>
-                        <p className='font-medium text-[24px]'>{getProfile?.data?.name}</p>
-                        <p className='font-medium '>{getProfile?.data?.email}</p>
-                        <p className='flex items-center gap-1'><CiStar size={22}/> {getProfile?.data?.rating}</p>
-                        <p>Total Car : {getProfile?.data?.carCount}</p>
-                    </div>
-                </div>
-                <div>
-                    <Link className='flex items-center bg-black text-white px-4 py-2 rounded-sm gap-1 justify-center  mt-5 md:mt-0' href={''} >Add a new car <IoAdd /> </Link>
-                </div>
-            </div>
-
-            <div className='mt-10'>
-                <Tabs defaultValue="car" className="w-full">
-                    <TabsList className="grid grid-cols-4 md:w-[70%] w-full gap-5 mb-10">
-                        <TabsTrigger value="car" className='bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]'>Car</TabsTrigger>
-                        <TabsTrigger  value="requested" className='bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]'>Requested</TabsTrigger>
-                        <TabsTrigger value="history" className='bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]'>History</TabsTrigger>
-                        <TabsTrigger value="earning" className='bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]'>Earning</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="car">
-                        <HostCars />
-                    </TabsContent>
-                    <TabsContent value="requested">
-                        <HostOrder/>
-                    </TabsContent>
-                    <TabsContent value="history">
-                        <HostHistoryInfo/>
-                    </TabsContent>
-                    <TabsContent value="earning">
-                       <HostEarning/>
-                    </TabsContent>
-                </Tabs>
-            </div>
+  const { data: getProfile } = useGetProfileQuery(undefined);
 
 
+  return (
+    <div className="my-10 container mx-auto px-2 md:px-0">
+      <div className="md:flex items-center  justify-between ">
+        <div className="flex items-center gap-5">
+          <Image
+            className="rounded-full h-20 w-20 object-cover"
+            src={`${imageUrl}/${getProfile?.data?.profile_image}`}
+            height={200}
+            width={200}
+            alt="img"
+          />
+          <div className="font-lora space-y-1">
+            <p className="font-medium text-[24px]">{getProfile?.data?.name}</p>
+            <p className="font-medium ">{getProfile?.data?.email}</p>
+            <p className="flex items-center gap-1">
+              <CiStar size={22} /> {getProfile?.data?.rating}
+            </p>
+            <p>Total Car : {getProfile?.data?.carCount}</p>
+          </div>
         </div>
-    )
-}
+        <div>
+          <Link
+            className="flex items-center bg-black text-white px-4 py-2 rounded-sm gap-1 justify-center  mt-5 md:mt-0"
+            href={""}
+          >
+            Add a new car <IoAdd />{" "}
+          </Link>
+        </div>
+      </div>
 
-export default HostHistoryPage
+      <div className="mt-10 ">
+        <Tabs defaultValue="car" className="w-full">
+          <TabsList className="grid grid-cols-3 md:grid-cols-6  w-full gap-5 mb-20">
+            <TabsTrigger
+              value="car"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Car
+            </TabsTrigger>
+            <TabsTrigger
+              value="requested"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Requested
+            </TabsTrigger>
+            <TabsTrigger
+            
+              value="ongoing"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Ongoing
+            </TabsTrigger>
+            <TabsTrigger
+              value="canceled"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Canceled
+            </TabsTrigger>
+            <TabsTrigger
+              value="completed"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Completed
+            </TabsTrigger>
+            <TabsTrigger
+              value="earning"
+              className="bg-white data-[state=active]:text-black  data-[state=active]:bg-[#0CFEE8]"
+            >
+              Earning
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="car">
+            <HostCars />
+          </TabsContent>
+          <TabsContent value="requested">
+            <HostOrder />
+          </TabsContent>
+          <TabsContent value="ongoing">
+            <OngoingOrder/>
+          </TabsContent>
+          <TabsContent value="history">
+            <HostHistoryInfo />
+          </TabsContent>
+          <TabsContent value="earning">
+            <HostEarning />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+
+export default HostHistoryPage;
---
+++ src/components/OngoingOrder/OngoingOrder.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import img from "../../assets/car.jpg";
+import Image from "next/image";
+import { CiLocationOn } from "react-icons/ci";
+import {
+    useAcceptCarRentRequestMutation,
+  useGetHostTripsQuery,
+} from "@/redux/Api/hostHistoryApi";
+import { imageUrl } from "@/redux/baseApi";
+import { toast } from "sonner";
+const OngoingOrder = () => {
+  // ALL APIs
+  const { data: getHosOngoingTrip } = useGetHostTripsQuery("ongoing");
+  const [acceptCarRequest] = useAcceptCarRentRequestMutation();
+
+
+
+    const handleCompleteTrip = (id : string)=>{
+        const data = {
+              tripId: id,
+              status: "completed",
+            };
+            acceptCarRequest(data)
+              .unwrap()
+              .then((payload) => {
+                toast.success(payload?.message)
+              })
+              .catch((error) => toast.error(error?.data?.message));
+    }
+
+
+
+  return (
+    <div className="font-lora">
+      <p className="pb-5">Request order </p>
+      {getHosOngoingTrip?.data?.trips?.map((order: any) => {
+        return (
+          <div key={order?._id} className="grid grid-cols-1 md:grid-cols-6 gap-10 border-b pb-8 mb-8">
+            <div className="col-span-1">
+              <Image
+                alt="img"
+                className="w-full"
+                height={300}
+                width={300}
+                src={img}
+              />
+            </div>
+            <div className="md:col-span-4 col-span-1">
+              <div className="md:flex  gap-10">
+                <div className="space-y-2">
+                  <p className="text-xl font-medium">
+                    {order?.car?.make} {order?.car?.model} {order?.car?.year}
+                  </p>
+                  <p>
+                    Start Date : {order?.tripEndDate} Time :{" "}
+                    {order?.tripEndDateTime?.split("T")[1]}
+                  </p>
+                  <p>
+                    End Date : {order?.tripStartDate} Time :{" "}
+                    {order?.tripStartDateTime?.split("T")[1]}
+                  </p>
+                  <p>
+                    <span className="text-[#528AAE] ml-2">
+                      Price : £ {order?.tripPrice}
+                    </span>
+                  </p>
+                </div>
+                <div>
+                  <p>Renter:</p>
+                  <div className="flex mt-2 gap-2">
+                    <Image
+                      alt="img"
+                      className="h-12 w-12 rounded-full"
+                      height={300}
+                      width={300}
+                      src={`${imageUrl}/${order?.user?.profile_image}`}
+                    />
+                    <div>
+                      <p>{order?.user?.name}</p>
+                      <p className="flex items-center gap-2">
+                        <CiLocationOn /> {order?.user?.address}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-10 space-y-3">
+                <p>
+                  Delivery Location :{" "}
+                  <span>{order?.pickupLocation || "Not Available"}</span>
+                </p>
+                <p>
+                  Return Location : <span>{order?.returnLocation}</span>
+                </p>
+              </div>
+            </div>
+            <div className=" col-span-1 space-x-2">
+              <p
+                className="bg-[#91BAD6] inline-block px-4 py-2 rounded-sm cursor-pointer"
+                onClick={() => handleCompleteTrip(order?._id)}
+              >
+                Complete
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default OngoingOrder;
